9e00ad3a447c4f6465e5517eafc8f830f29c4cdd
pavelshoba <pavel.shoba@monterosa.co.uk>
2022-01-30 14:27:08 +0300

    Classwork9 error1

--- src/test/java/lib/ui/mobile_web/MWArticlePageObject.java
+++ src/test/java/lib/ui/mobile_web/MWArticlePageObject.java
@@ -8,8 +8,8 @@ public class MWArticlePageObject extends ArticlePageObject
     static {
         TITLE = "css:#content h1";
         FOOTER_ELEMENT = "css:footer";
-        OPTIONS_ADD_TO_MY_LIST_BUTTON = "css:#page-actions li#ca-watch button";
-        OPTIONS_REMOVE_FROM_MY_LIST_BUTTON = "css://a[@title='Remove this page from your watchlist']";
+        OPTIONS_ADD_TO_MY_LIST_BUTTON = "css:a#ca-watch";
+        OPTIONS_REMOVE_FROM_MY_LIST_BUTTON = "xpath:a[@title='Remove this page from your watchlist']";
     }
 
     public MWArticlePageObject(RemoteWebDriver driver)
